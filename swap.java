4cf14fb773995c760fa8c222f70b857edb2210a7
Siddhi <patilsiddhi156@gmail.com>
2026-01-19 19:13:26 +0530

    Swapping two no.

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap{
+public static void main(String[] args){
+int num1 = 2;
+int num2 = 4;
+int temp;
+System.out.println("Before Swapping :" +num1+" " +num2);
+
+temp = num1;
+num1 = num2;
+num2 = temp;
+System.out.println("After Swapping :" +num1+" " +num2);
+  }
+} 
