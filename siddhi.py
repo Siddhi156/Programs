80630d59315833404692eb2afae3230ffa74cd9a
Siddhi <patilsiddhi156@gmail.com>
2026-01-19 18:31:14 +0530

    added siddhi.py file to git for testing

---
+++ siddhi.py
@@ -0,0 +1,22 @@
+#print(11,12,13,14)
+#name = "siddhi"
+#rollno = 131 
+#print("my name is",name)
+#print("my roll no. is"+str(rollno))
+
+# marks=45
+# if per>=0 and per<40:
+# print("pass")
+# else:
+# print("fail")
+
+print("\U0001F92A",end=" ")
+print("\U0001F637",end=" ")
+print("\U0001F62A")
+print("\U0001F928",end=" ")
+print("\U0001F600")
+
+import calendar
+yy=2025
+mm=9
+print(calendar.month(yy, mm))
